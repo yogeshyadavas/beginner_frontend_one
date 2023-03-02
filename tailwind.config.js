b0f578999776b40141152313321c5fffa1c23d93
Yogesh Yadav <75256665+yogeshyadavas@users.noreply.github.com>
2023-03-02 18:54:18 +0530

    added fonts & screen breackpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+          'system-ui',
+        ],
+      },
+    },
+    screens: {
+      'sm' : '576px',
+      'md' : '768px',
+      'lg' : '992px',
+      'xl': '1200px',
+    },
   },
   plugins: [],
 }
