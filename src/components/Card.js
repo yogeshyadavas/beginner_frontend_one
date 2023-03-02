62bf332a59f9f45ca01c0a65b7ac8025eca92a05
Yogesh Yadav <75256665+yogeshyadavas@users.noreply.github.com>
2023-03-02 18:28:13 +0530

    added data to component & CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,37 +20,48 @@ function Card() {
   // Return the JSX element
   return (
     <>
-      <div className="flex">
-        <div>
-          <img
-            src="https://avatars.dicebear.com/v2/avataaars/Bret.svg?options[mood][]=happy"
-            alt="Avatar"
-            width={200}
-            height={200}
-          />
-        </div>
-        <div className="bg-blue-300">
-          <h2>Leanne Graham {users.name}</h2>
-          <p>
-            <strong>Email: </strong>Sincere@april.biz
-          </p>
-          <p>
-            <strong>Phone: </strong>1-770-736-8031 x56442
-          </p>
-          <p>
-            <strong>Company: </strong>Romaguera-Crona
-          </p>
-          <p>
-            <strong>Website: </strong>hildegard.org
-          </p>
-          <p>
-            <strong>Address: </strong>Kulas Light, Apt. 556, Gwenborough,
-            92998-3874
-          </p>
-        </div>
-      </div>
+      {DataisLoaded ? (
+        users.map((user) => (
+          <div key={user.id} className="card-align shadow-card">
+            <div className="row">
+              <div className="col-auto">
+                <img
+                  src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+                  alt="Avatar"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div className="col">
+                <h2 className="mb-[15px] text-[2rem] font-medium leading-[1.2]">
+                  {user.name}
+                </h2>
+                <p className="mb-[5px]">
+                  <strong>Email:</strong> {user.email}
+                </p>
+                <p className="mb-[5px]">
+                  <strong>Phone:</strong> {user.phone}
+                </p>
+                <p className="mb-[5px]">
+                  <strong>Company:</strong> {user.company.name}
+                </p>
+                <p className="mb-[5px]">
+                  <strong>Website:</strong> {user.website}
+                </p>
+                <p className="mb-[5px]">
+                  <strong>Address:</strong> {user.address.street},{" "}
+                  {user.address.suite}, {user.address.city},{" "}
+                  {user.address.zipcode}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </>
   );
-};
+}
 
 export default Card;
